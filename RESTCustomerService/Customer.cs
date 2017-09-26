b5f0205a79083341b783227230f08eee455e282a
Dshy <KortSkg@KORTSKG-PC>
2017-09-26 13:15:00 +0200

    Det er gemt

---
+++ RESTCustomerService/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RESTCustomerService
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Year { get; set; }
+        public int Id { get; set; }
+
+        public Customer()
+        {
+
+        }
+        public Customer(int id, string firstname, string lastname, int year)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Year = year;
+            Id = id;
+        }
+    }
+}
--- RESTCustomerService/Service1.svc.cs
+++ RESTCustomerService/Service1.svc.cs
@@ -12,22 +12,13 @@ namespace RESTCustomerService
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        private static List<Customer> cList = new List<Customer>();
+        public IList<Customer> GetCustomer()
         {
-            return string.Format("You entered: {0}", value);
+            cList.Add(new Customer(1, "Mig", "Selv", 1000));
+           
+            return cList;
         }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
+        
     }
 }
